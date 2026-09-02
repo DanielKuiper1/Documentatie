96150673c2456e0d69a78cf8d531b30f2d9e2b0f
danielkuiper1 <contact@danielkuiper.nl>
2026-09-02 21:08:36 +0200

    edited base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/Documentatie/',
   plugins: [vue()],
   resolve: {
     alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
